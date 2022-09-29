5fcce8a47698c1c7b740e9976e17fe5bc0f0a08b
Drew Miranda <drew.miranda@graylog.com>
2022-09-29 16:51:23 -0500

    very crude first draft

---
+++ src/web.py
@@ -0,0 +1,78 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from unittest import result
+from urllib.parse import urlparse
+import time
+import json
+
+hostName = "localhost"
+serverPort = 8080
+
+def get_ipv4_by_hostname(hostname):
+    import socket
+    rs = socket.gethostbyname(hostname)
+    return rs
+
+def get_domain_name(ip_address):
+  import socket
+  result=socket.gethostbyaddr(ip_address)
+  return list(result)[0]
+
+def lookupRDns(argQuery):
+    result = get_domain_name(argQuery)
+    return result
+
+def lookupDns(argQuery):
+    result = get_ipv4_by_hostname(argQuery)
+    return result
+    # return result
+
+def parseArgs(argQuery):
+    dictArgs = {}
+    sKey = ""
+    sVal = ""
+
+    oSplit = argQuery.split("&")
+    for sArg in oSplit:
+        # print(sArg)
+        oKv = sArg.split("=")
+        sKey = oKv[0]
+        sVal = oKv[1]
+        dictArgs[sKey] = sVal
+    return dictArgs
+
+def doLookups(argQuery):
+    oArgs = parseArgs(argQuery)
+    sLookup = oArgs['lookup']
+    if sLookup == "rdns":
+        return lookupRDns(oArgs['key'])
+    elif sLookup == "dns":
+        return lookupDns(oArgs['key'])
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        # self.wfile.write(bytes("%s" % self.path, "utf-8"))
+        # print(self.path)
+        o = urlparse(self.path)
+        if o.path == "/":
+            dictRs = {}
+            # print("path is / lets do cool stuff")
+            rs = doLookups(o.query)
+            dictRs['value'] = rs
+            y = json.dumps(dictRs)
+
+            self.wfile.write(bytes(y, "utf-8"))
+
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
