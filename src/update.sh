263b110ffaf8aaf008a75b782af457e68d559d2a
Drew Miranda <drew.miranda@graylog.com>
2023-02-01 15:06:53 -0600

    fix owner when file are downloaded via root user

--- src/update.sh
+++ src/update.sh
@@ -3,6 +3,8 @@ echo Downloading updated web.py file
 sudo wget https://raw.githubusercontent.com/drewmiranda-gl/gl-lookups/main/src/web.py -O /opt/graylog/lookup-service/web.py
 sudo wget https://raw.githubusercontent.com/drewmiranda-gl/gl-lookups/main/src/dns_ip_search.py -O /opt/graylog/lookup-service/dns_ip_search.py
 
+sudo chown -R gl_lookup_service:gl_lookup_service /opt/graylog/lookup-service/
+
 echo Restarting gl_lookup.service
 sudo systemctl restart gl_lookup.service
 
