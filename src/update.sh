69682e5496c62cf1590eac5854621d0dc9b0b0db
Drew Miranda <drew.miranda@graylog.com>
2023-02-01 11:11:17 -0600

    update dns crawler script as well

--- src/update.sh
+++ src/update.sh
@@ -1,6 +1,7 @@
 # download
 echo Downloading updated web.py file
 sudo wget https://raw.githubusercontent.com/drewmiranda-gl/gl-lookups/main/src/web.py -O /opt/graylog/lookup-service/web.py
+sudo wget https://raw.githubusercontent.com/drewmiranda-gl/gl-lookups/main/src/dns_ip_search.py -O /opt/graylog/lookup-service/dns_ip_search.py
 
 echo Restarting gl_lookup.service
 sudo systemctl restart gl_lookup.service
