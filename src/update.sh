11c936fdf3f655dba82ae3a07280d75a72160928
Drew Miranda <drew.miranda@graylog.com>
2023-02-01 16:26:20 -0600

    delete log file on update

--- src/update.sh
+++ src/update.sh
@@ -6,10 +6,18 @@ sudo wget https://raw.githubusercontent.com/drewmiranda-gl/gl-lookups/main/src/d
 sudo chown -R gl_lookup_service:gl_lookup_service /opt/graylog/lookup-service/
 
 echo Restarting gl_lookup.service
-sudo systemctl restart gl_lookup.service
+sudo systemctl stop gl_lookup.service
+sleep 1
+rm -f /opt/graylog/lookup-service/web1.log
+sleep 1
+sudo systemctl start gl_lookup.service
 
 echo Waiting 60 seconds for service to restart
 sleep 60
 
 echo Restarting gl_lookup2.service
-sudo systemctl restart gl_lookup2.service
+sudo systemctl stop gl_lookup2.service
+sleep 1
+rm -f /opt/graylog/lookup-service/web2.log
+sleep 1
+sudo systemctl start gl_lookup2.service
