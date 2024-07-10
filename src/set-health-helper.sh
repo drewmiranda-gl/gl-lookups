12fce5bd70952b3353efb4767bffb5b109437532
Drew Miranda <drew.miranda@graylog.com>
2024-07-10 17:17:00 -0400

    lower sleep time and add note about why this value is set the way it is

--- src/set-health-helper.sh
+++ src/set-health-helper.sh
@@ -5,4 +5,8 @@ file=$1
 value=$2
 
 echo -n $2 > $1
-sleep 60
+# it is important this allows enough time for the HA proxy (or other load
+#   balancer) to execute its health check and mark this as DOWN before
+#   the service actually fully exits and stops accetping requests
+#   currently doing 1 check every 1s so 10s should be plenty
+sleep 10
